deb01c5288d92dd4734ba3f52ea8e2367d6836b6
JayaSurya12345 <jayasurya.n2021csea@sece.ac.in>
2023-07-18 23:09:15 +0530

    Register Unregister Added in Manage Component

--- EventIn/FrontEnd/src/components/Manage/Manage.jsx
+++ EventIn/FrontEnd/src/components/Manage/Manage.jsx
@@ -1,36 +1,50 @@
-import React from 'react'
-import './Manage.css'
+import React from "react";
+import "./Manage.css";
 
 import Close from "./Close.png";
 import eventImage from "./noprofil.jpg";
-import { useState, useEffect} from 'react';
-
+import { useState, useEffect } from "react";
 
 const Manage = () => {
- 
   const [isPopupVisible, setPopupVisible] = useState(false);
-  const [eventsVisible, setEventsVisible] =useState(false);
-  const eventsList=[{date:"26/10/2003",title:"techno 2k23",description:"This is an technical symposium conducted by sri eshwar college of engineering to teach students about the technologies"},
-  {date:"26/10/2003",title:"techno 2k23",description:"This is an technical symposium conducted by sri eshwar college of engineering to teach students about the technologies"},
-  {date:"26/10/2003",title:"techno 2k23",description:"This is an technical symposium conducted by sri eshwar college of engineering to teach students about the technologies"}]
-  
-    const [images, setImages] = useState([]) ;
-    const [imageURLs, setImageURLs] = useState([]) ;
-    useEffect ( () => {
-    if (images. length < 1) return;
+  const [eventsVisible, setEventsVisible] = useState(false);
+  const eventsList = [
+    {
+      date: "26/10/2003",
+      title: "techno 2k23",
+      description:
+        "This is an technical symposium conducted by sri eshwar college of engineering to teach students about the technologies",
+    },
+    {
+      date: "26/10/2003",
+      title: "techno 2k23",
+      description:
+        "This is an technical symposium conducted by sri eshwar college of engineering to teach students about the technologies",
+    },
+    {
+      date: "26/10/2003",
+      title: "techno 2k23",
+      description:
+        "This is an technical symposium conducted by sri eshwar college of engineering to teach students about the technologies",
+    },
+  ];
+
+  const [images, setImages] = useState([]);
+  const [imageURLs, setImageURLs] = useState([]);
+  useEffect(() => {
+    if (images.length < 1) return;
     const newImageUrls = [];
-    images. forEach (image => newImageUrls.push (URL. createObjectURL (image) )) ;
-    setImageURLs (newImageUrls);
-     }, [images]);
+    images.forEach((image) => newImageUrls.push(URL.createObjectURL(image)));
+    setImageURLs(newImageUrls);
+  }, [images]);
 
-    function onImageChange (e) {
-    setImages ([...e.target.files]);
-    }
-
+  function onImageChange(e) {
+    setImages([...e.target.files]);
+  }
 
   const handleButtonClick = () => {
-    if(eventsVisible){
-      setEventsVisible(false)
+    if (eventsVisible) {
+      setEventsVisible(false);
     }
     setPopupVisible(true);
   };
@@ -38,83 +52,100 @@ const Manage = () => {
   const handleCloseClick = () => {
     setPopupVisible(false);
   };
-  
-  const displayEventsClick=()=>{
-    if(isPopupVisible){
+
+  const displayEventsClick = () => {
+    if (isPopupVisible) {
       setPopupVisible(false);
     }
     setEventsVisible(true);
   };
-  const closeEventsClick=()=>{
-     setEventsVisible(false);
+
+  const [isRegistered, setIsRegistered] = useState(false);
+  const handleClick = () => {
+    setIsRegistered(!isRegistered);
   };
 
   return (
-    <div className='header'>
-    <div className='overall-wrapper'>
-      <div className="events-btn" onClick={displayEventsClick}>
-           <div>View All</div>
-      </div>
-      <div className="events-btn" onClick={handleButtonClick}>
+    <div className="header">
+      <div className="overall-wrapper">
+        <div className="events-btn" onClick={displayEventsClick}>
+          <div>View All</div>
+        </div>
+        <div className="events-btn" onClick={handleButtonClick}>
           <div>Add Events</div>
-      </div>
-      <div className="box">
+        </div>
+        <div className="box">
           <input type="text" placeholder="Search here..."></input>
           <a href="#">
             <i class="fas fa-search"></i>
           </a>
         </div>
-    </div>
-    <div>
+      </div>
+      <div>
         {isPopupVisible && (
-        <div className="popup">
-          <div className="popup-content">
-            <img
-              src={Close}
-              alt="Close"
-              className="close"
-              onClick={handleCloseClick}
-            ></img>
-            { imageURLs.map (imageSrc => <img src={imageSrc}/>) }
-            <input type="file" multiple accept=" image/*" onChange={onImageChange}/>
-            <input type="text" placeholder="Event Name"></input>
-            <input type="date" placeholder="Date"></input>
-            <textarea name="paragraph_text" placeholder="Description about the event" cols="37" rows="8"></textarea>
-             <div className='add-event-btn'>Add Event + </div>
-          </div>
-        </div>
-      )}
-      {
-        eventsVisible && (
-                <div onClick={closeEventsClick}>
-                  <div  className='events-list'>
-                    {eventsList.map((item,index)=>{
-                      return(
-                  <div className="feedback-item">
-                          <div className="feedback-top">
-                            <div className="feedback-username">
-                              <h3>{item.title}</h3>
-                             </div>
-                           <div className="feedback-date">
-                     <h5>{item.date}</h5>
-                </div>
-                </div>
-              <div className="feedback-text">
-                <h4>
-                 {item.description}
-                </h4>
-              </div>
+          <div className="popup">
+            <div className="popup-content">
+              <img
+                src={Close}
+                alt="Close"
+                className="close"
+                onClick={handleCloseClick}
+              ></img>
+              {imageURLs.map((imageSrc) => (
+                <img src={imageSrc} />
+              ))}
+              <input
+                type="file"
+                multiple
+                accept=" image/*"
+                onChange={onImageChange}
+              />
+              <input type="text" placeholder="Event Name"></input>
+              <input type="date" placeholder="Date"></input>
+              <textarea
+                name="paragraph_text"
+                placeholder="Description about the event"
+                cols="37"
+                rows="8"
+              ></textarea>
+              <div className="add-event-btn">Add Event + </div>
             </div>
-                  )
-              })
-            }
-              </div>
-             </div>
-        )
-      }
-        </div>
+          </div>
+        )}
+        {eventsVisible && (
+          <div>
+            <div className="events-list">
+              {eventsList.map((item, index) => {
+                return (
+                  <div className="feedback-item">
+                    <div className="feedback-top">
+                      <div className="feedback-username">
+                        <h3>{item.title}</h3>
+                      </div>
+                      <div className="feedback-date">
+                        <h5>{item.date}</h5>
+                      </div>
+                    </div>
+                    <div className="feedback-text">
+                      <h4>{item.description}</h4>
+                    </div>
+                    <div className="register-unregister">
+                      <button
+                        className="register-unregister-button"
+                        onClick={handleClick}
+                      >
+                        {isRegistered ? "Unregister" : "Register"}
+                      </button>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Manage
+export default Manage;
